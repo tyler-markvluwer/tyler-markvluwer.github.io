714b92ee02a6636ad8565591c6c458fc59b08de4
Tyler Markvluwer <vluwer@Tylers-MBP.home>
2016-04-25 00:09:17 -0700

    auto update

---
+++ publish.sh
@@ -0,0 +1,7 @@
+rm -rf ../tyler-markvluwer.github.io/*
+cp -r * ../tyler-markvluwer.github.io/
+cd ../tyler-markvluwer.github.io/
+git add --all
+git commit -m "auto update"
+git push origin master
+cd -
